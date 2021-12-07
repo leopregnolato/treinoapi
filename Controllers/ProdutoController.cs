19829241e72c146fbfa652cfc2e35fed4e48dc28
lopl <lopl@gft.com>
2021-12-07 17:02:36 -0300

    Cria a controller Produto e as primeiras rotas

--- Controllers/ProdutoController.cs
+++ Controllers/ProdutoController.cs
@@ -2,11 +2,23 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 
 namespace treinoapi.Controllers
 {
-    public class ProdutoController
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProdutoController : ControllerBase
     {
+        [HttpGet]
+        public IActionResult PegarProdutos(){
+            return Ok(new {nome = "Leo Pregnolato", empresa = "GFT"});
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult PegarProdutos(int id){
+            return Ok("Leo Pregnolato" + id);
+        }
         
     }
 }
