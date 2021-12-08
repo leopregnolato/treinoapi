550334876bbdd888e3566dd1da27578605f56605
lopl <lopl@gft.com>
2021-12-08 10:08:54 -0300

    Inclui a primeira requisição POST que envia para o banco de dados

--- Controllers/ProdutoController.cs
+++ Controllers/ProdutoController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using treinoapi.Data;
+using treinoapi.Models;
 
 namespace treinoapi.Controllers
 {
@@ -10,6 +12,11 @@ namespace treinoapi.Controllers
     [ApiController]
     public class ProdutoController : ControllerBase
     {
+        private readonly ApplicationDbContext database;
+        public ProdutoController(ApplicationDbContext database){
+            this.database = database;
+        }
+
         [HttpGet]
         public IActionResult PegarProdutos(){
             return Ok(new {nome = "Leo Pregnolato", empresa = "GFT"});
@@ -18,6 +25,22 @@ namespace treinoapi.Controllers
         [HttpGet("{id}")]
         public IActionResult PegarProdutos(int id){
             return Ok("Leo Pregnolato" + id);
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] ProdutoTemp pTemp){
+            Produto p = new Produto();
+            p.Nome = pTemp.Nome;
+            p.Preco = pTemp.Preco;
+            database.Produtos.Add(p);
+            database.SaveChanges();
+
+            return Ok(new {msg = "Você criou um novo produto:"});
+        }
+
+        public class ProdutoTemp{
+            public string Nome { get; set; }
+            public float Preco { get; set; }
         }
         
     }
